b66ca164b280363811ee9419e854b71b3dc0ea1d
Tobi Michael <tapuboi@gmail.com>
2025-03-11 23:27:44 +0100

    Create test.py

---
+++ test.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+# Set the title of the web app
+st.title("Love Messenger ❤️")
+
+# Add a header
+st.header("A Special Message")
+
+# Display the message
+st.write("💖 I love you 💖")
