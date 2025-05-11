4dbafd02bfa1d76d2b33de17dfc265d88a902fa7
Tobi Michael <tapuboi@gmail.com>
2025-05-11 05:05:50 +0100

    Update streamlittest.py
    added 2 sidebars

--- streamlittest.py
+++ streamlittest.py
@@ -1,12 +1,10 @@
 import streamlit as st
 
-# 1. Title
-st.title("Streamlit Layout Test")
+# --- Main Body ---
+st.title("Streamlit Layout Test with Sidebar")
+st.write("This is a simple layout test to demonstrate Streamlit columns and a sidebar.")
 
-# 2. Description
-st.write("This is a simple layout test to demonstrate Streamlit columns.")
-
-# 3. Divide body into 3 columns
+# Divide body into 3 columns
 col1, col2, col3 = st.columns(3)
 
 # Place content in each column
@@ -18,3 +16,18 @@ with col2:
 
 with col3:
     st.write("I love you dearly")
+
+# --- Left Sidebar ---
+# Streamlit only supports one sidebar, which is on the left
+with st.sidebar:
+    st.write("input sidebar")
+    # You can add more elements here, e.g.,
+    # st.button("Click me")
+    # st.slider("Select a value", 0, 100)
+
+# --- Regarding a Right Sidebar ---
+# Streamlit does NOT have a built-in right sidebar feature like st.sidebar.
+# Achieving a panel on the right requires different layout techniques
+# using columns or containers in the main body, but it won't behave
+# like a collapsible sidebar.
+# For the purpose of this script, we will only use the standard left sidebar.
